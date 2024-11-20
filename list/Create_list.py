810b3717dfb1c533a8b2aaf0235e5689362f6784
Aswathi Biju <aswathibiju8118@gmail.com>
2024-11-20 21:47:52 +0530

    Create Create_list.py

---
+++ list/Create_list.py
@@ -0,0 +1,4 @@
+""" Task: Create a list of your favorite fruits. Print the first and last fruits in the list."""
+
+favourite_fruits=["Apple","Orange","Pear","Pineapple","Banana"]
+print(favourite_fruits[0],favourite_fruits[-1])
