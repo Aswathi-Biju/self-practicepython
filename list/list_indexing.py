a48a983222b962371cdf4464bdb595f262b63d6c
Aswathi Biju <aswathibiju8118@gmail.com>
2024-11-20 21:51:31 +0530

    Create list_indexing.py

---
+++ list/list_indexing.py
@@ -0,0 +1,5 @@
+""" Task: Given a list of numbers [10, 20, 30, 40, 50], print the second, fourth, and last elements."""
+numbers=[10,20,30,40,50]
+print(numbers[1])
+print(numbers[3])
+print(numbers[-1])
